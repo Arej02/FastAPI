f823132821f41e071aee4899083a4c50bfb37c3f
Arya Raj Khadka <159859689+Arej02@users.noreply.github.com>
2025-12-24 14:26:06 +1030

    added compare and stock route

--- electronics/app.py
+++ electronics/app.py
@@ -69,4 +69,90 @@ def smart_filer(
 
     }
 
+@app.get("/stock")
+def get_stock(threshold:int | None =Query(
+    None,description="Enter the max number of stocks you want:")):
+
+    data=load_data(file_path)
+
+    result=[]
+
+    for data_id,data_items in data.items():
+        stock=data_items.get("stock",0)
+        if stock==0 or (threshold is not None and data_items.get("stock")<threshold): 
+            result.append({data_id:data_items})
+
+    return {
+        "count":len(result),
+        "devices":result
+    }
+
+@app.get("/compare/{brand1}/{brand2}") 
+def compare_brand(
+    brand1:str=Path(
+        ...,title="Brand 1",
+        description="Enter the first brand",example="Dell"),
+    brand2:str=Path(
+        ...,title="Brand 2",
+        description="Enter the second brand",example="Apple"
+    )):
+
+    data=load_data(file_path)
+    allowed_brands=['apple','samsung','dell','sony']
+    brand1=brand1.strip().lower()
+    brand2=brand2.strip().lower()
+
+    if brand1 not in allowed_brands or brand2 not in allowed_brands:
+        raise HTTPException(status_code=404, detail="Brand not found")
+
+    stats={
+        brand1:{"price":[],"rating":[]},
+        brand2:{"price":[],"rating":[]}
+    }
+
+    for device in data.values():
+        brand=device.get("brand","").lower().strip()
+
+        if brand in stats:
+            stats[brand]["price"].append(device.get("price",0))
+            stats[brand]["rating"].append(device.get("rating",0))
+
+    def cal_mean(values: list[float]) -> float:
+        if not values:
+            return 0
+        return round(sum(values) / len(values), 2)
+    
+    avg_price_1 = cal_mean(stats[brand1]["price"])
+    avg_price_2 = cal_mean(stats[brand2]["price"])
+
+    premium_brand = (
+        brand1 if avg_price_1 > avg_price_2 else brand2
+        if avg_price_2 > avg_price_1 else "equal"
+    )
+
+    return {
+        "brand_1": {
+            "name": brand1,
+            "average_price": avg_price_1,
+            "average_rating": cal_mean(stats[brand1]["rating"]),
+            "product_count": len(stats[brand1]["price"])
+        },
+        "brand_2": {
+            "name": brand2,
+            "average_price": avg_price_2,
+            "average_rating": cal_mean(stats[brand2]["rating"]),
+            "product_count": len(stats[brand2]["price"])
+        },
+        "premium_heavy_brand": premium_brand
+    }
+
+
+
+
+    
+    
+
+
+
+
 
